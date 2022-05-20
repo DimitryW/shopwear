0e05ea86b49e6f69869632c686d83a2d3211bfa0
Dimitry Wu <grayfen@gmail.com>
2022-05-20 20:03:12 +0800

    add Sigin-with Goole

--- app.py
+++ app.py
@@ -142,5 +142,5 @@ def api_product(product_id):
 
 
 if __name__ == "__main__":
-    # app.run(host='0.0.0.0', port=3000)
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=3000)
+    # app.run(debug=True, port=5000)
