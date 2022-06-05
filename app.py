a86718545d79b3525e8645b3024378ecbe12709f
Dimitry Wu <grayfen@gmail.com>
2022-06-05 22:56:38 +0800

    .

--- app.py
+++ app.py
@@ -622,7 +622,6 @@ def api_mywear():
 def api_wear_detail(wear_id):
     (data, member_data, product_photos) = Wears.show_wear_detail(wear_id)
     # (data, product_photos) = Wears.show_wear_detail(wear_id)
-    member_data = Wears.show_mem_detail(data[0][2])
     res = {
     "id": data[0][0],
     "photo":[str(data[0][2]) +"/"+ data[0][1]],
